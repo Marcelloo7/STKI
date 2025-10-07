0ce6848b1c4b270d17d115814af65255674170da
Atanasius Marcello Avta Pendhi Putra <marcello.pendhia@gmail.com>
2025-10-07 20:47:07 +0700

    Menambahkan file proyek STKI

---
+++ Pertemuan 2/panah.py
@@ -0,0 +1,18 @@
+#clue
+
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
+
+print("2"*2)
+print(2*2)
+
+print(type("2"))
+print(type(2))
---
+++ Pertemuan 2/try.py
@@ -0,0 +1,11 @@
+print("Hello World!@#!#!$")
+print("test")
+
+print("Hello, Python!")
+print("Cello")
+print('Cello')
+
+print()
+
+print("Saya sudah sarapan tadi pagi\n dan nanti siang makan lagi")
+print("Saya sudah sarapan tadi pagi\t dan nanti siang makan lagi")
